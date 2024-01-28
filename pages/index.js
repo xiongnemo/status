2b8823c0eb134aeb7337097117a38d16c4da79cc
Nemo Xiong <xiongnemo@126.com>
2024-01-28 16:00:26 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -96,9 +96,9 @@ export default function Index({ config, kvMonitors, kvMonitorsLastUpdate }) {
             </a>
             &{' '}
             <a href="https://flareact.com/" target="_blank">
-              Flareact, {' '}
+              Flareact{' '}
             </a>
-            a part of{' '}
+            ,{' '}part of{' '}
             <a href="https://anzupop.com/" target="_blank">
               Anzupop{' '}
             </a>
