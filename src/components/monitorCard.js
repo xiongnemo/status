49020f9b81db51f92ae846721b505e28db3d5ad2
Nemo Xiong <xiongnemo@126.com>
2024-02-24 19:18:35 +1100

    Update monitorCard.js

--- src/components/monitorCard.js
+++ src/components/monitorCard.js
@@ -51,7 +51,7 @@ export default function MonitorCard({ key, monitor, data }) {
       <MonitorHistogram monitorId={monitor.id} kvMonitor={data} />
 
       <div className="flex flex-row justify-between items-center text-gray-400 text-sm">
-        <div>{config.settings.daysInHistogram} days ago</div>
+        <div>{config.settings.daysInHistogram} 天之前</div>
         <div>Today</div>
       </div>
     </div>
